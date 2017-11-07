00ec9953877e7313429b3f48b6329cf16708911e
Umberto Palo <u.palo@U-PALO.replynet.prv>
2017-11-07 17:29:19 +0100

    primo commit

---
+++ demoSpringBootLog4j2/src/main/java/it/umberto/palo/DemoSpringBootLog4j2Application.java
@@ -0,0 +1,12 @@
+package it.umberto.palo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootLog4j2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootLog4j2Application.class, args);
+	}
+}
