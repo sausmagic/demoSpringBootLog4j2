b40dc7629837666284f5d837823aa3a048d0ec86
Umberto Palo <u.palo@U-PALO.replynet.prv>
2017-11-08 13:30:51 +0100

    ok

--- demoSpringBootLog4j2/src/main/java/it/umberto/palo/DemoSpringBootLog4j2Application.java
+++ demoSpringBootLog4j2/src/main/java/it/umberto/palo/DemoSpringBootLog4j2Application.java
@@ -2,10 +2,12 @@ package it.umberto.palo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class DemoSpringBootLog4j2Application {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringBootLog4j2Application.class, args);
 	}
--- demoSpringBootLog4j2/src/main/java/it/umberto/palo/controller/IndexController.java
+++ demoSpringBootLog4j2/src/main/java/it/umberto/palo/controller/IndexController.java
@@ -3,17 +3,19 @@ package it.umberto.palo.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
     @RequestMapping("/")
-    String index(){
+    String index(ModelMap model){
         logger.debug("This is a debug message");
         logger.info("This is an info message");
         logger.warn("This is a warn message");
         logger.error("This is an error message");
+        
         return "index";
     }
 }
